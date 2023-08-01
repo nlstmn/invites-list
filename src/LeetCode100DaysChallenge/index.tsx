acd2b93fedaaae5248d68ee47b2ed99f8907482c
Liana NS <nlstmn@gmail.com>
2023-08-01 20:58:47 +0300

    Array Prototype Last TS

--- src/LeetCode100DaysChallenge/index.tsx
+++ src/LeetCode100DaysChallenge/index.tsx
@@ -9,8 +9,20 @@ interface Props {
     onClose: () => void;
 }
 
+declare global { // Module scope for global declaration
+    interface Array<T> { // T stands for Type
+        last(): T | -1;
+    }
+}
+
 export function LeetCode100DaysChallenge() {
-    console.log("For debugging")
+
+    Array.prototype.last = function () {
+        return (
+            this.length === 0 ? -1 : this[this.length - 1] // Accessing array using "this"
+        )
+    }
+
     return (
         <>
             <div>test</div>
