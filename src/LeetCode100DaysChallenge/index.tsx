9d1622b076a03215429f9e9c78219edaaae68450
Liana NS <nlstmn@gmail.com>
2023-08-02 13:43:40 +0300

    Two sum

--- src/LeetCode100DaysChallenge/index.tsx
+++ src/LeetCode100DaysChallenge/index.tsx
@@ -1,14 +1,5 @@
 import React, { Component, ReactNode } from "react";
 
-interface State {
-    opened: boolean;
-}
-
-interface Props {
-    opened: boolean;
-    onClose: () => void;
-}
-
 declare global { // Module scope for global declaration
     interface Array<T> { // T stands for Type
         last(): T | -1;
@@ -17,11 +8,27 @@ declare global { // Module scope for global declaration
 
 export function LeetCode100DaysChallenge() {
 
+    /* Array Prototype Last */
     Array.prototype.last = function () {
         return (
             this.length === 0 ? -1 : this[this.length - 1] // Accessing array using "this"
         )
     }
+
+    /* Two Sum */
+    function twoSum(nums: number[], target: number) {
+
+        let output: number[] = [];
+
+        for (let i = 0; i < nums.length; i++) {
+            for (let j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] === target) {
+                    output.push(i, j);
+                    return output;
+                }
+            }
+        }
+    };
 
     return (
         <>
