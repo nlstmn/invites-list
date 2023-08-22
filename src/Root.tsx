88cc1e3b40aa5cac61a1a7230c01ad1ea4ad0a2d
Liana NS <nlstmn@gmail.com>
2023-08-22 16:41:52 +0300

    Packages updated and MUI installed

--- src/Root.tsx
+++ src/Root.tsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Modal } from "./Modal";
 import { Invites } from "./Invites";
 import { LeetCode100DaysChallenge } from "./LeetCode100DaysChallenge"
+import Button from '@mui/material/Button';
 
 interface State {
   invites: string[];
@@ -32,12 +33,13 @@ export class Root extends Component<{}, State> {
         <button onClick={() => this.toggle(true)}>
           Open list
         </button>
-        <Modal opened={this.state.opened} onClose={() => this.toggle(false)}>
+        {/*<Modal opened={this.state.opened} onClose={() => this.toggle(false)}>
           <Invites
             invites={this.state.invites}
             onAdd={this.invite.bind(this)}
           />
-        </Modal>
+        </Modal>*/}
+        <Button variant="contained">Hello world</Button>
       </>
     );
   }
