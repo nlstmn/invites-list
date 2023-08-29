73a1d20ae925a33cd902b42ee7a90bcdcfd755dd
Liana NS <nlstmn@gmail.com>
2023-08-29 20:39:38 +0300

    Testing

--- src/LeetCode100DaysChallenge/partTwo.tsx
+++ src/LeetCode100DaysChallenge/partTwo.tsx
@@ -2,6 +2,8 @@ import React, { FC, useState, useCallback, useEffect, useRef } from "react";
 
 export function PartTwo() {
 
+    console.log("Part Two")
+
     return (
         <>
             <div>2nd Script</div>
