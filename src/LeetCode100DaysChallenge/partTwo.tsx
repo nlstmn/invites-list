b224f30a9f4cfbbca50538d829ab778a4ca61922
Liana NS <nlstmn@gmail.com>
2023-08-29 23:41:22 +0300

    useEffect hook added

--- src/LeetCode100DaysChallenge/partTwo.tsx
+++ src/LeetCode100DaysChallenge/partTwo.tsx
@@ -3,10 +3,19 @@ import React, { FC, useState, useCallback, useEffect, useRef } from "react";
 export function PartTwo() {
 
     console.log("Part Two")
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        console.log("OHA! ", count)
+    });
 
     return (
         <>
             <div>2nd Script</div>
+            <div>
+                <p>Count: {count}</p>
+                <button onClick={() => setCount(count + 1)}>Increment</button>
+            </div>
         </>
     );
 }
