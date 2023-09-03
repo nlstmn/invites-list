c11ff50f265185405ccb3a40e570395601ce0b8d
Liana NS <nlstmn@gmail.com>
2023-09-03 23:24:45 +0300

    Generics logic

--- src/LeetCode100DaysChallenge/partTwo.tsx
+++ src/LeetCode100DaysChallenge/partTwo.tsx
@@ -8,8 +8,17 @@ export function PartTwo() {
         return { ...obj, uid };
     }
 
-    let person = addUID({ name: "Liana", age: 28 }); // This is called invoking
+    let person = addUID({ name: "Lia", age: 28 }); // This is called invoking
     // console.log(person.name) - Will give error because addUID doesn't know what exactly will exist in object
+
+    // Same func, but with Generics logic
+    const UIDgenerics = function <T>(obj: T) { // T can be any name btw
+        let uid = Math.floor(Math.random() * 100);
+        return { ...obj, uid };
+    }
+
+    let person2 = UIDgenerics({ name: "Didi", age: 35 }); // This is called invoking
+    console.log(person2.name)
 
     return (
         <>
