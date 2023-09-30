f099a8cbfc7faa053b200e7092df4eb46e048f01
Liana NS <nlstmn@gmail.com>
2023-09-30 16:06:36 +0300

    Wrapping Root in QueryClientProvider for useQuery

--- src/LeetCode100DaysChallenge/reactQuery.tsx
+++ src/LeetCode100DaysChallenge/reactQuery.tsx
@@ -3,6 +3,21 @@ import { useQuery } from "react-query"; // Automatic caching, no need of manual 
 
 export function ReactQuery() {
 
+    async function fetchDataFunc() {
+        try {
+            const apiUrl = 'https://jsonplaceholder.typicode.com/posts';
+            const response = await fetch(apiUrl);
+            if (!response.ok) {
+                throw new Error(`Failed to fetch data. Status: ${response.status}`);
+            }
+            const data = await response.json();
+            return data;
+        } catch (error) {
+            console.error('An error occurred while fetching data:', error);
+            throw error;
+        }
+    }
+
     /* 1 - Query for fetching data instead of useEffect hook */
     const { data, isLoading } = useQuery("myData", fetchDataFunc);
 
--- src/Root.tsx
+++ src/Root.tsx
@@ -8,11 +8,14 @@ import { VirtualScrolling } from "./LeetCode100DaysChallenge/virtualScrolling"
 import { ReactQuery } from "./LeetCode100DaysChallenge/reactQuery"
 import Button from '@mui/material/Button';
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
 
 interface State {
   invites: string[];
   opened: boolean;
 }
+
+const queryClient = new QueryClient()
 
 export class Root extends Component<{}, State> {
   public readonly state: State = {
@@ -34,49 +37,51 @@ export class Root extends Component<{}, State> {
   public render() {
     return (
       <>
-        <Router>
-          <>
-            <nav>
-              <ul>
-                <li>
-                  <Link to="/">Home</Link>
-                </li>
-                <li>
-                  <Link to="/challenge">Challenge</Link>
-                </li>
-                <li>
-                  <Link to="/part-two">Part Two</Link>
-                </li>
-                <li>
-                  <Link to="/manychat">Manychat</Link>
-                </li>
-                <li>
-                  <Link to="/virtualscrolling">Virtual Scrolling</Link>
-                </li>
-                <li>
-                  <Link to="/rquery">Virtual Scrolling</Link>
-                </li>
-              </ul>
-            </nav>
+        <QueryClientProvider client={queryClient}>
+          <Router>
+            <>
+              <nav>
+                <ul>
+                  <li>
+                    <Link to="/">Home</Link>
+                  </li>
+                  <li>
+                    <Link to="/challenge">Challenge</Link>
+                  </li>
+                  <li>
+                    <Link to="/part-two">Part Two</Link>
+                  </li>
+                  <li>
+                    <Link to="/manychat">Manychat</Link>
+                  </li>
+                  <li>
+                    <Link to="/virtualscrolling">Virtual Scrolling</Link>
+                  </li>
+                  <li>
+                    <Link to="/rquery">React Query</Link>
+                  </li>
+                </ul>
+              </nav>
 
-            <Routes>
-              <Route path="/challenge" element={<LeetCode100DaysChallenge />} />
-              <Route path="/part-two" element={<PartTwo />} />
-              <Route path="/" element={<Button variant="contained" onClick={() => this.toggle(true)}>Open list</Button>}>
-              </Route>
-              <Route path="/manychat" element={<Manychat />} />
-              <Route path="/virtualscrolling" element={<VirtualScrolling />} />
-              <Route path="/rquery" element={<ReactQuery />} />
-            </Routes>
+              <Routes>
+                <Route path="/challenge" element={<LeetCode100DaysChallenge />} />
+                <Route path="/part-two" element={<PartTwo />} />
+                <Route path="/" element={<Button variant="contained" onClick={() => this.toggle(true)}>Open list</Button>}>
+                </Route>
+                <Route path="/manychat" element={<Manychat />} />
+                <Route path="/virtualscrolling" element={<VirtualScrolling />} />
+                <Route path="/rquery" element={<ReactQuery />} />
+              </Routes>
 
-            {/*<Modal opened={this.state.opened} onClose={() => this.toggle(false)}>
+              {/*<Modal opened={this.state.opened} onClose={() => this.toggle(false)}>
             <Invites
               invites={this.state.invites}
               onAdd={this.invite.bind(this)}
             />
             </Modal>*/}
-          </>
-        </Router>
+            </>
+          </Router>
+        </QueryClientProvider>
       </>
     );
   }
