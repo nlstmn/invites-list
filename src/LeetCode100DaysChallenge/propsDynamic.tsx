457b17201c30de1c1fb2101c1d42fdeae4a849a0
Liana NS <nlstmn@gmail.com>
2023-10-09 19:03:39 +0300

    Explanation

--- src/LeetCode100DaysChallenge/propsDynamic.tsx
+++ src/LeetCode100DaysChallenge/propsDynamic.tsx
@@ -1,5 +1,9 @@
 import React, { Component, useEffect, useState } from "react";
 
+/* The term "render prop" refers to a function that a component
+receives as a prop and uses to render its content, allowing you
+to share functionality and data between components. */
+
 function DataFetcher({ render }: any) {
     const [data, setData] = useState(null)
     useEffect(() => {
